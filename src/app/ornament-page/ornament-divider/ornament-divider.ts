ec4eaebf4f37fd43f679bec2e945549fd2185cef
DESKTOP-8USVFTG\Admin <patrykzbiciak@onet.pl>
2026-09-10 22:09:38 +0200

    feat: enhance ornament divider styles and update SVG structure for improved visuals

--- src/app/ornament-page/ornament-divider/ornament-divider.ts
+++ src/app/ornament-page/ornament-divider/ornament-divider.ts
@@ -7,35 +7,24 @@ import { Component, input } from '@angular/core';
     template: ` <div class="divider-container" [class.flip]="direction() === 'down'">
         <ng-container *ngTemplateOutlet="rhombusTemplate; context: { flip: true }" />
         <ng-container *ngTemplateOutlet="lineTemplate" />
-        <svg class="ornament-svg" viewBox="0 0 150 50">
+        <svg class="ornament-svg" viewBox="0 -45 150 110">
             <path
                 class="ornament-path"
-                d="M 5 0
-                L 25 0
-                L 75 50 
-                L 125 0
-                L 145 0
-                M 5 0
-                L 0 0
-                L 30 30 
-                L 75 -10 
-                L 120 30 
-                L 150 0
-                L 145 0"
+                d="M 5 10 L 25 10 L 75 60 L 125 10 L 145 10 M 5 10 L 0 10 L 30 40 L 75 0 L 120 40 L 150 10 L 145 10"
             />
         </svg>
         <ng-container *ngTemplateOutlet="lineTemplate" />
-        <ng-container *ngTemplateOutlet="rhombusTemplate; context: { direction: 'right' }" />
+        <ng-container *ngTemplateOutlet="rhombusTemplate" />
 
         <ng-template #lineTemplate>
             <svg class="line-svg" preserveAspectRatio="none" viewBox="0 0 100 2">
-                <line x1="0" y1="0" x2="100" y2="0" />
+                <line x1="0" y1="1" x2="100" y2="1" />
             </svg>
         </ng-template>
 
         <ng-template #rhombusTemplate let-flip="flip">
-            <svg class="rhombus-svg" [class.flip-x]="flip" viewBox="0 0 30 25">
-                <path d="M 0 0 L 10 -12.5 L 30 0 L 10 12.5 Z"/>
+            <svg class="rhombus-svg" [class.flip-x]="flip" viewBox="0 0 35 25">
+                <path d="M 0 12.5 L 10 0 L 30 12.5 L 10 25 Z" />
             </svg>
         </ng-template>
     </div>`,
