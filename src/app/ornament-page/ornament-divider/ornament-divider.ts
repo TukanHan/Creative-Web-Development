a6a47ab5ee3c0cca8d2a6a8348d1b3479f6c6f62
DESKTOP-8USVFTG\Admin <patrykzbiciak@onet.pl>
2026-09-10 19:29:43 +0200

    refactor: simplify SVG structure and update styles for ornament divider

--- src/app/ornament-page/ornament-divider/ornament-divider.ts
+++ src/app/ornament-page/ornament-divider/ornament-divider.ts
@@ -5,40 +5,12 @@ import { Component, input } from '@angular/core';
     selector: 'app-ornament-divider',
     imports: [NgTemplateOutlet],
     template: ` <div class="divider-container" [class.flip]="direction() === 'down'">
-        <ng-container *ngTemplateOutlet="rhombusTemplate; context: { direction: 'left' }" />
-        <ng-container *ngTemplateOutlet="lintTemplate" />
+        <ng-container *ngTemplateOutlet="rhombusTemplate; context: { flip: true }" />
+        <ng-container *ngTemplateOutlet="lineTemplate" />
         <svg class="ornament-svg" viewBox="0 0 150 50">
-            <path class="ornament-path" [attr.d]="pathData" />
-        </svg>
-        <ng-container *ngTemplateOutlet="lintTemplate" />
-        <ng-container *ngTemplateOutlet="rhombusTemplate; context: { direction: 'right' }" />
-
-        <ng-template #lintTemplate>
-            <svg class="line-svg" preserveAspectRatio="none" viewBox="0 0 100 2">
-                <line x1="0" y1="0" x2="100" y2="0" />
-            </svg>
-        </ng-template>
-
-        <ng-template #rhombusTemplate let-direction="direction">
-            <svg class="rhombus-svg" viewBox="0 0 30 25">
-                <g [attr.transform]="direction === 'left' ? 'rotate(180 15 0)' : null">
-                    <path
-                        d="M 0 0 
-                        L 10 -12.5 
-                        L 30 0 
-                        L 10 12.5 
-                        Z"
-                    />
-                </g>
-            </svg>
-        </ng-template>
-    </div>`,
-    styleUrl: './ornament-divider.css',
-})
-export class OrnamentDivider {
-    public readonly direction = input<'up' | 'down'>('up');
-
-    readonly pathData = `M 5 0
+            <path
+                class="ornament-path"
+                d="M 5 0
                 L 25 0
                 L 75 50 
                 L 125 0
@@ -49,5 +21,26 @@ export class OrnamentDivider {
                 L 75 -10 
                 L 120 30 
                 L 150 0
-                L 145 0`;
+                L 145 0"
+            />
+        </svg>
+        <ng-container *ngTemplateOutlet="lineTemplate" />
+        <ng-container *ngTemplateOutlet="rhombusTemplate; context: { direction: 'right' }" />
+
+        <ng-template #lineTemplate>
+            <svg class="line-svg" preserveAspectRatio="none" viewBox="0 0 100 2">
+                <line x1="0" y1="0" x2="100" y2="0" />
+            </svg>
+        </ng-template>
+
+        <ng-template #rhombusTemplate let-flip="flip">
+            <svg class="rhombus-svg" [class.flip-x]="flip" viewBox="0 0 30 25">
+                <path d="M 0 0 L 10 -12.5 L 30 0 L 10 12.5 Z"/>
+            </svg>
+        </ng-template>
+    </div>`,
+    styleUrl: './ornament-divider.css',
+})
+export class OrnamentDivider {
+    public readonly direction = input<'up' | 'down'>('up');
 }
